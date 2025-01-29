547eca5806aba18d6255c70e2ae3fc7451994622
AbdulRehmanAnsari35 <scurbgaming94@gmail.com>
2025-01-29 23:46:08 +0500

    Your commit message

--- src/app/Stripe-Payment/page.tsx
+++ src/app/Stripe-Payment/page.tsx
@@ -6,7 +6,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
 if (process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY === undefined) {
-    throw new Error('NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY is not defined')
+    throw new Error('STRIPE_PUBLISHABLE_KEY is not defined')
 }
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
